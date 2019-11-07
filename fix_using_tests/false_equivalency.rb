4d92fc057cb5918fb41d3f7fdf3886b93344f59c
Michael Gerland <mr.gerland@gmail.com>
2019-11-07 18:21:15 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -17,11 +17,11 @@ def selection(num)
      "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
      "HAM HAM HAM IN MY TUMMY"
-  binding.pry
   end
 end
 
 def runner
   prompt_user
   selection(get_user_input)
+  binding.pry
 end
