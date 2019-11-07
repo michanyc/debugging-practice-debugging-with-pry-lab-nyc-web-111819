60d09da076d6e4d0249ba2c4db168661d9400d5d
Michael Gerland <mr.gerland@gmail.com>
2019-11-07 16:53:21 +0000

    Automatically backed up by Learn

--- first_priority/that_was_unexpected.rb
+++ first_priority/that_was_unexpected.rb
@@ -4,8 +4,8 @@ class Person
   attr_accessor :name, :candy_lover
 
   def initialize(name, candy_lover)
-    name = name
-    candy_lover = candy_lover
+    name == name
+    candy_lover == candy_lover
       binding.pry
   end
 
