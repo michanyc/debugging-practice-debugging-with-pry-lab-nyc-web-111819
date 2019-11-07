e3803ccc5530c9b7b261d8e9a5141429c1f207ea
Michael Gerland <mr.gerland@gmail.com>
2019-11-07 19:39:50 +0000

    Done.

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,9 +1,12 @@
+require 'pry'
 def generate_star_date
+
   (rand(100000) + 400000) / 10.0
 end
 
 
 def state_log(star_date)
+
   "Captain's Log, star date #{star_date}."
 end
 
@@ -11,8 +14,9 @@ def crew
   ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
 end
 
-def greet_crew (crew)
-  crew.each {|crew_member| "Hello #{crew_member}."}
+def greet_crew(crew)
+
+  crew.map {|crew_member| "Hello #{crew_member}."}
 end
 
 def engage
