6be108ed3b1269012a5e07101f74763cb991813f
Michael Gerland <mr.gerland@gmail.com>
2019-11-07 19:14:37 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -15,10 +15,10 @@ def crew
 end
 
 def greet_crew(crew)
-  binding.pry
+
   crew.each {|crew_member| "Hello #{crew_member}."}
 end
-
+binding.pry
 def engage
   date = generate_star_date
   state_log(star_date)
