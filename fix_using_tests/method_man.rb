73f928044ba1aaf412e04193b19656c15bd3eb1c
Michael Gerland <mr.gerland@gmail.com>
2019-11-07 18:39:36 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,11 +1,12 @@
 require 'pry'
 
 def start_game(player1, player2)
+  binding.pry
   "Hello #{player1} & #{playar2}"
-binding.pry
+
 end
 
 def play_game(player2)
   "#{player1} is better than #{player2}"
-
+binding.pry
 end
