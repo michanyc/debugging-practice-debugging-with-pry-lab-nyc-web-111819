9da027578414353a395fae079b5c249da2c4d53a
Michael Gerland <mr.gerland@gmail.com>
2019-11-07 16:41:50 +0000

    Automatically backed up by Learn

--- first_priority/you_called.rb
+++ first_priority/you_called.rb
@@ -2,8 +2,8 @@ require "pry"
 
 
 def super_mario_party
-  binding.pry
-  "Sorry, your princess is in another castle"
 
+  "Sorry, your princess is in another castle"
+binding.pry
 end
 super_mario_party
